62bc26bac09870f7919fd882c5f40180d7ec0ac9
johanschamp <johan.schamp@gmail.com>
2024-02-26 17:55:29 +0100

    Initial commit

---
+++ src/test/java/be/thomasmore/gunranch/GunranchApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.gunranch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GunranchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
